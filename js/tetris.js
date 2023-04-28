f2298e0e82133e4b5a952ce9ba8d85732ea7c273
Kaito-Aoyama <115942498+Kaito-Aoyama@users.noreply.github.com>
2023-04-28 15:54:15 +0900

    ミノの色を変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'lavender', 'olive', 'blue', 'yellow', 'deep pink', 'lime', 'purple'
+    'skyblue', 'olive', 'lavender', 'yellow', 'plum', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
